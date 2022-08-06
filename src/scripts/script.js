ef8a74678c8c4a392e4050315a996fb16ac859a2
Sergo <cergotabagua222@gmail.com>
2022-08-06 17:06:57 +0300

    Add touchstart & click events

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -7,22 +7,26 @@ const randomPosition = () => {
     return { x, y }
 }
 
-const buttons = document.querySelectorAll('.button')
+const editButtonPosition = (button) => {
+    const { x, y } = randomPosition();
+    console.log(x, y);
+    button.style.position = 'absolute';
+    button.style.left = `${x}px`;
+    button.style.top = `${y}px`;
+}
+
+const buttons = document.querySelectorAll('.button');
 buttons.forEach(button => {
     switch (button.dataset.action) {
         case 'editTitle':
             button.addEventListener('click', () => {
                 document.querySelector('.question__title').innerHTML = button.dataset.title
-            })
-            break;
-        case 'editButtonPosition':
-            button.addEventListener('mouseover', () => {
-                const { x, y } = randomPosition();
-                console.log(x, y);
-                button.style.position = 'absolute';
-                button.style.left = `${x}px`;
-                button.style.top = `${y}px`;
             });
             break;
+        case 'editButtonPosition':
+            button.ontouchstart = editButtonPosition.bind(null, button);
+            button.onclick = editButtonPosition.bind(null, button);
+            button.onmouseenter = editButtonPosition.bind(null, button);
+            break;
     }
-})
+});
